5bed13871d3d42cd2aee97477475be1126b49a27
Joseph2702 <119374135+Joseph2702@users.noreply.github.com>
2024-05-21 18:54:05 +0700

    resident controller

---
+++ app/Http/Controllers/ResidentsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests\StoreprodukRequest;
+use App\Models\residents;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ResidentsController extends Controller
+{
+   public function index()
+   {
+    $data['residents'] = residents::all();
+    return view('pages.admin-pages.home-admin', $data);
+   }
+
+   public function create()
+   {
+       return view('pages.admin-pages.home-admin');
+   }
+
+   public function store(Request $request)
+   {
+       $data['residents'] = residents::all();
+       $input = $request->all();
+       residents::create($input);
+       return view('pages.admin-pages.home-admin', $data);
+   }
+
+   public function show($id)
+   {
+       $residents = residents::findOrFail($id);
+       return view('pages.admin-pages.home-admin', compact('residents'));
+   }
+
+
+}
